192321235b46d9f803ee8efd35b66ba291b53f06
MrSquirrel228 <League@iMac-3.attlocal.net>
2018-06-13 19:57:40 -0700

    DiceRoll complete/ RockPaperScissors complete/ Validation WIP

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -21,8 +21,15 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 3. Use each value of randomNumber to give the user a random compliment.
+if(randomNumber==0){	JOptionPane.showMessageDialog(null,"You have eyes");
 
-		// 4. Repeat all the code above 10 times
+}else if (randomNumber==1){JOptionPane.showMessageDialog(null,"You are a human");
+		
+}else if (randomNumber==2){JOptionPane.showMessageDialog(null,"You have a right hand");
+
+}else if (randomNumber==3){JOptionPane.showMessageDialog(null,"You have a left hand");
+}
+// 4. Repeat all the code above 10 times
 		
 		// 5. Find someone to test out your program. They will like it :)
 	}
