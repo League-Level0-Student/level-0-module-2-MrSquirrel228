6784b6f2c043c809d2250edb397f6f649f52e377
League <League@AdminisorsiMac2.attlocal.net>
2018-06-18 16:58:13 -0700

    Validation complete/ LotteryNumbers WIP

---
+++ src/random/LotteryNumbers.java
@@ -0,0 +1,11 @@
+package random;
+
+import java.util.Random;
+
+public class LotteryNumbers {
+	public static void main(String[] args) {
+
+		Random gen = new Random (); 
+		int r1 = gen .nextInt((100)+1);
+		
+	}}
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -11,7 +11,7 @@ public class Validation {
 	public static void main(String[] args) {
 
 		// 1. OPTIONAL: Watch the first 2 minutes of this movie: http://vimeo.com/2485018
-		
+		for (int i = 1; i < 11; i++){
 		Random randomMaker = new Random();
 		
 		int randomNumber = randomMaker.nextInt(5);
@@ -21,6 +21,8 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 3. Use each value of randomNumber to give the user a random compliment.
+		
+		
 if(randomNumber==0){	JOptionPane.showMessageDialog(null,"You have eyes");
 
 }else if (randomNumber==1){JOptionPane.showMessageDialog(null,"You are a human");
@@ -28,7 +30,7 @@ if(randomNumber==0){	JOptionPane.showMessageDialog(null,"You have eyes");
 }else if (randomNumber==2){JOptionPane.showMessageDialog(null,"You have a right hand");
 
 }else if (randomNumber==3){JOptionPane.showMessageDialog(null,"You have a left hand");
-}
+}}
 // 4. Repeat all the code above 10 times
 		
 		// 5. Find someone to test out your program. They will like it :)
