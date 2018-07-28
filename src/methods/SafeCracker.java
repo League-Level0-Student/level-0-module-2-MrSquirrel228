9eee0135f5d53a17b6e566c50966dd0963d02b97
MrSquirrel228 <league@iMac.attlocal.net>
2018-07-28 11:31:00 -0700

    SafeCracker complete/RollerCoaster wip

--- src/methods/SafeCracker.java
+++ src/methods/SafeCracker.java
@@ -4,7 +4,6 @@ import java.applet.AudioClip;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.util.Random;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -18,14 +17,18 @@ public class SafeCracker {
 		 * Your mission: use the tryCode method to crack the safe by trying all possible
 		 * combinations
 		 */
-for (int i = 1; i <1000000; i++) {
-	//tryCode();
+
+		
+		
+		
+for (int i = 1; i <10000000; i++) {
+	tryCode(i);
 }
 
 	}
 
 	static void tryCode(int guess) {
-		System.out.println("trying " + guess);
+		
 
 		int secretCode = 9999999 - wekncrzpasfdkjhcfjse;
 
---
+++ src/random/rollerCoaster.java
@@ -0,0 +1,10 @@
+package random;
+
+import javax.swing.JOptionPane;
+
+public class rollerCoaster {
+public static void main(String[] args) {
+	
+	JOptionPane.showInputDialog("How tall are you in inches?");
+	
+}}
